d82e0d58a7f8e8db370d0a710e155b20a7159edd
oluiscabral <oluiscabral@hotmail.com>
2023-10-03 01:35:56 -0300

    Add console logs

--- src/main.ts
+++ src/main.ts
@@ -24,21 +24,25 @@ const timekeeper = new Timekeeper(workdayDataAccess, employeeDataAccess, breakDa
 
 app.command('/hi', async ({command, ack}) => {
     await ack();
+    console.log(command);
     await timekeeper.shiftStart({date: new Date(), employeeId: command.user_id});
 });
 
 app.command('/bye', async ({command, ack}) => {
     await ack();
+    console.log(command);
     await timekeeper.shiftEnd({date: new Date(), employeeId: command.user_id});
 });
 
 app.command('/brb', async ({command, ack}) => {
     await ack();
+    console.log(command);
     await timekeeper.breakStart({date: new Date(), employeeId: command.user_id});
 });
 
 app.command('/back', async ({command, ack}) => {
     await ack();
+    console.log(command);
     await timekeeper.breakEnd({date: new Date(), employeeId: command.user_id});
 });
 
